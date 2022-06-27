125b0313a4966c4fa949f5b6814298a643453b82
Akif Islam <iamakifislam@gmail.com>
2022-06-28 02:34:15 +0600

    Lifecycle Observation

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -79,7 +79,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
-        dessertTimer = DessertTimer()
+        
+        dessertTimer = DessertTimer(this.lifecycle);
 
         // Set the TextViews to the right values
         binding.revenue = revenue
